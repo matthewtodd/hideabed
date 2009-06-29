f935927228916bfe0650c1c2621493ec9344dbff
Matthew Todd <matthew.todd@gmail.com>
2009-06-29 16:07:55 +0300

    Push view lookup into database.

--- app/controllers/views_controller.rb
+++ app/controllers/views_controller.rb
@@ -1,23 +1,13 @@
 class ViewsController < ApplicationController
   before_filter :load_database
-  before_filter :load_design
-  before_filter :load_view
 
   def show
-    render :json => @database.documents.view(@view)
+    render :json => @database.view(params[:design_name], params[:name])
   end
 
   private
 
   def load_database
     @database = Database.find_by_name!(params[:database_name])
-  end
-
-  def load_design
-    @design = @database.documents.find_by_name!("_design/#{params[:design_name]}")
-  end
-
-  def load_view
-    @view = @design.views.find_by_name!(params[:name])
   end
 end
--- app/models/database.rb
+++ app/models/database.rb
@@ -4,6 +4,12 @@ class Database < ActiveRecord::Base
   validates_format_of :name, :with => %r{^[a-z0-9_$()+-/]+$}
   validates_uniqueness_of :name
 
+  def view(design_name, view_name)
+    design = documents.find_by_name!("_design/#{design_name}")
+    view   = design.views.find_by_name!(view_name)
+    documents.view(view)
+  end
+
   def to_json
     { :db_name => name }.to_json
   end
