59493a11fd71ee032d188ab0b505616ff2e27426
Matthew Todd <matthew.todd@gmail.com>
2009-06-30 11:57:07 +0300

    Clean up params parser.

--- config/initializers/params_parser.rb
+++ config/initializers/params_parser.rb
@@ -2,10 +2,6 @@
 ActionController::ParamsParser
 
 # ...so we can override its json handling
-ActionController::Base.param_parsers[Mime::JSON] = Proc.new do |raw|
-  if raw.blank?
-    { :json => {} }
-  else
-    { :json => Doily(raw).with_indifferent_access }
-  end
+ActionController::Base.param_parsers[Mime::JSON] = lambda do |request_body|
+  { :json => Doily(request_body).with_indifferent_access }
 end
